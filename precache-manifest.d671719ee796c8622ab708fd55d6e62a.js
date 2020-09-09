836a32a980f1651ec6b180d48839e29230a4ae0f
Natalia <nataliarudomyr1@gmail.com>
2020-09-09 22:35:51 +0300

    Updates

---
+++ precache-manifest.d671719ee796c8622ab708fd55d6e62a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55df16fd1b5843ba464819350ae48d4a",
+    "url": "/hw16/index.html"
+  },
+  {
+    "revision": "0675b9caab95df8b9799",
+    "url": "/hw16/static/css/main.97b33c5e.chunk.css"
+  },
+  {
+    "revision": "c328065bf0bd77e04990",
+    "url": "/hw16/static/js/2.aa533778.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hw16/static/js/2.aa533778.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0675b9caab95df8b9799",
+    "url": "/hw16/static/js/main.f0463a07.chunk.js"
+  },
+  {
+    "revision": "c9402234c36bd9c46363",
+    "url": "/hw16/static/js/runtime-main.1c6d2d5a.js"
+  },
+  {
+    "revision": "321b22feaf715adfea76bd59240b74c5",
+    "url": "/hw16/static/media/female-icon.321b22fe.jpg"
+  },
+  {
+    "revision": "592396bc148a5dbf49971ff36ffbf1bf",
+    "url": "/hw16/static/media/male-icon.592396bc.jpg"
+  },
+  {
+    "revision": "ab7f36c566c2b80cb351ae3a550527a8",
+    "url": "/hw16/static/media/unknown.ab7f36c5.png"
+  }
+]);
