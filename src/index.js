12a1894ff3529cfb405a6d3c82f34025e4fd2c85
Natalia <nataliarudomyr1@gmail.com>
2020-09-09 22:34:48 +0300

    hw16

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import PhoneBook from './App';
+import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <PhoneBook />
   </React.StrictMode>,
   document.getElementById('root')
 );
